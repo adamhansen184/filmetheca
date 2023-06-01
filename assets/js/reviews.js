3fa1ae7201c997316ced160e73c170c868e77c8e
Ryan Mattson <ryan.mattson32@gmail.com>
2023-05-31 19:54:55 -0600

    added code for js file and more css

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -1,1 +1,18 @@
+// API KEY
 key = "https://www.omdbapi.com/db5d550d";
+
+
+// References
+let movieNameRef = document.getElementById("Movie-Name");
+let searchbutton = document.getElementById("search-button");
+let result = document.getElementById("result");
+
+// Functions to use API
+let getMvoie= () => {
+    let movieNameRef = movieNameRef.value;
+    let url= 'http://www.omdbapi.com/?t=${movieName}&apikey${key}';
+    if(movieName.lenght <= 0){
+        result.innerHTML
+    }
+};
+

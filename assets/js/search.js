7313622e8481cc34eebe6d1a724e5215fa9fcfc2
Adam Hansen <adamhansen@me.com>
2023-06-01 20:00:20 -0600

    Remove support for multiple result pages

--- assets/js/search.js
+++ assets/js/search.js
@@ -27,7 +27,7 @@ var configurationTMDB = JSON.parse(localStorage.getItem('TMDBConfiguration'));
 // Declare and initialize a variable as a string to store the secure base image URL of the images provided by the TMDB API.
 var imageBaseUrlTMDB = configurationTMDB.images.secure_base_url;
 // Declare and initialize a variable as a number to store the first subsequent page number to search with the TMDB API.
-var pageTMDB = 2;
+var pageTMDB = 1;
 // Declare and initialize a variable as a string to store the query to search with the TMDB API.
 var queryTMDB = 'Lord of the Rings';
 // Declare a variable to store the results of the search with the TMDB API.
@@ -40,39 +40,16 @@ var titleIdTMDB;
 
 // Implement a fetch call that will search using the TMDB API for a queried movie/show.
 // TODO: Wrap fetch call in a function called by a search box, drop-down selection, and search button.
+// TODO: Support multiple pages of results for a queried movie/show by implementing a button to display the next page of results.
+fetch(`https://api.themoviedb.org/3/search/${searchTypeTMDB}?api_key=${apiKeyTMDB}&query=${encodeURIComponent(queryTMDB)}&include_adult=false&language=en-US&page=${pageTMDB}`)
+    .then( function(response) { return response.json() } )
+    .then( function(data) { resultsTMDB = data.results } );
+
 // TODO: Parse the results of the search and display a list of matching movies/shows to the user.
 // TODO: For each movie/show in the list, display the title and poster image.
 // TODO: For each movie/show in the list, include a button to add the movie/show to the user's list of movies/shows to watch, storing the TMDB ID in local storage.
 // TODO: For each movie/show in the list, include a button to add the movie/show to the user's list of watched movies/shows, storing the TMDB ID in local storage.
 // TODO: For each movie/show in the list, include a drop-down menu to rate a watched movie/show, storing the rating in local storage.
-fetch(`https://api.themoviedb.org/3/search/${searchTypeTMDB}?api_key=${apiKeyTMDB}&query=${encodeURIComponent(queryTMDB)}&include_adult=false&language=en-US`)
-    .then( function(initialResponse) { 
-        return initialResponse.json();
-    } )
-    .then( function(initialData) {
-        if (initialData.total_pages <= 1) {
-            // Store the results of the search as an array in resultsTMDB.
-            resultsTMDB = initialData.results;
-        } else {
-            // Store the results of the initial search as an array in resultsTMDB.
-            resultsTMDB = initialData.results;
-            // Do-while loop to fetch subsequent pages of results for a queried movie/show.
-            do {
-                // Implement a fetch call that will search using the TMDB API for subsequent pages of results for a queried movie/show, beginning at page 2.
-                fetch(`https://api.themoviedb.org/3/search/${searchTypeTMDB}?api_key=${apiKeyTMDB}&query=${encodeURIComponent(queryTMDB)}&include_adult=false&language=en-US&page=${pageTMDB}`)
-                    .then( function(subsequentResponse) {
-                        return subsequentResponse.json();
-                    } )
-                    .then( function(subsequentData) {
-                        // TODO: Append the results of the subsequent search(es) to the resultsTMDB array.
-                        console.log(subsequentData.results);
-                        // resultsTMDB = resultsTMDB.concat(subsequentResults); <-- This doesn't work.
-                    } );
-                // Increment the page number to search with the TMDB API.
-                pageTMDB++;
-            } while (pageTMDB <= initialData.total_pages);
-        }
-     } );
 
 // Declare and initialize a variable to store a listing of all free and subscription streaming services in the United States supported by the Watchmode API.
 // TODO: Implement a function called by a button that will refresh the list of streaming sources provided by the Watchmode API.

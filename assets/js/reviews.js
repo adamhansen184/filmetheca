a1d06fc1eeb3782f745ac023d03db428aa0c675e
Ryan Mattson <ryan.mattson32@gmail.com>
2023-06-01 18:42:07 -0600

    Added possible remaining js

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -11,8 +11,53 @@ let result = document.getElementById("result");
 let getMvoie= () => {
     let movieNameRef = movieNameRef.value;
     let url= 'http://www.omdbapi.com/?t=${movieName}&apikey${key}';
-    if(movieName.lenght <= 0){
-        result.innerHTML
-    }
+if (movieName.length <= 0) {
+    result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
+  }
+  //If input field is NOT empty
+  else {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        //If movie exists in database
+        if (data.Response == "True") {
+          result.innerHTML = `
+            <div class="info">
+                <img src=${data.Poster} class="poster">
+                <div>
+                    <h2>${data.Title}</h2>
+                    <div class="rating">
+                        <img src="star-icon.svg">
+                        <h4>${data.imdbRating}</h4>
+                    </div>
+                    <div class="details">
+                        <span>${data.Rated}</span>
+                        <span>${data.Year}</span>
+                        <span>${data.Runtime}</span>
+                    </div>
+                    <div class="genre">
+                        <div>${data.Genre.split(",").join("</div><div>")}</div>
+                    </div>
+                </div>
+            </div>
+            <h3>Plot:</h3>
+            <p>${data.Plot}</p>
+            <h3>Cast:</h3>
+            <p>${data.Actors}</p>
+            
+        `;
+        }
+        //If movie does NOT exists in database
+        else {
+          result.innerHTML = `<h3 class='msg'>${data.Error}</h3>`;
+        }
+      })
+      //If error occurs
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
+      });
+  }
 };
+searchBtn.addEventListener("click", getMovie);
+window.addEventListener("load", getMovie);
 

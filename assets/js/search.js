beafe8d89cda182dd45a53662c57d7c89966c461
Adam Hansen <adamhansen@me.com>
2023-06-03 10:55:47 -0600

    Add display of TMDB API search results

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,6 +4,8 @@
 const apiKeyTMDB = 'cac3d39cd60268fd28eb5af557322903';
 const apiKeyWatchmode = 'gbfUPl5ghxgo8Q0kxoP5vcmHOvg2IuVomZdxVJES';
 
+// Declare and initialize a variable to return the search results container in the DOM.
+var searchResultContainer = document.getElementById('searchResultContainer');
 // Declare and initialize a variable to return the title query field in the DOM.
 var titleQueryField = document.getElementById('titleQueryField');
 // Declare and initialize a variable to return the title search form in the DOM.
@@ -60,27 +62,88 @@ function queryTMDB(event) {
 }
 
 // Declare a function to parse the results of the search.
-// TODO: Display the results of matching movies/shows to the user.
+// Display the search results of matching movies/shows to the user, including title, poster image, and rating.
 // TODO: For each movie/show in the list, display the title, poster image, and rating.
 // TODO: For each movie/show in the list, include a button to add the movie/show to the user's list of movies/shows to watch, storing the data in local storage.
 // TODO: For each movie/show in the list, include a button to add the movie/show to the user's list of watched movies/shows, storing the data in local storage.
 // TODO: For each movie/show in the list, include a drop-down menu to rate a watched movie/show, storing the rating in local storage.
 // TODO: Support multiple pages of results for a queried movie/show by implementing a button to display the next page of results.
 function parseTMDBResults (results) {
-    console.log('TMDB Search Results');
-    console.log(results);
 
     for (var i = 0; i < results.length; i++) {
-        console.log('Title ID');
-        console.log(results[i].id);
-        console.log('Title');
-        console.log(results[i].title);
-        console.log('Poster Image');
-        console.log(results[i].poster_path);
-        console.log('Vote Average');
-        console.log(results[i].vote_average);
-        console.log('Vote Count');
-        console.log(results[i].vote_count);
+
+        // Declare and initialize a variable to store the current result object.
+        let result = results[i];
+
+        // Declare and initialize a variable to store and create a Bootstrap card HTML element.
+        let cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.classList.add('mb-3');
+        // Add the TMDB Title ID to the card HTML element ID attribute.
+        cardElement.setAttribute('id', result.id);
+        cardElement.setAttribute('style', 'max-width: 540px;'); // <-- This should most likely be moved into a CSS file instead of being inline.
+        // Declare and initialize a variable to store and create a Bootstrap card row HTML element.
+        let cardRowElement = document.createElement('div');
+        cardRowElement.classList.add('row');
+        cardRowElement.classList.add('g-0');
+        // Declare and initialize a variable to store and create a Bootstrap card image column HTML element.
+        let cardImageColumnElement = document.createElement('div');
+        cardImageColumnElement.classList.add('col-md-4');
+        // Declare and initialize a variable to store and create a Bootstrap card image HTML element.
+        let cardImageElement = document.createElement('img');
+        cardImageElement.classList.add('img-fluid')
+        cardImageElement.classList.add('rounded-start');
+        // Set the alt attribute of the card image HTML element to include the title of the movie/show.
+        cardImageElement.setAttribute('alt', `Poster Image for ${result.title}`);
+        // Add the TMDB Poster Image to the card image HTML element.
+        // TODO: Add a placeholder image if the TMDB Poster Image is not available.
+        cardImageElement.setAttribute('src', `${imageBaseUrlTMDB}w154${result.poster_path}`);
+        // Declare and initialize a variable to store and create a Bootstrap card body column HTML element.
+        let cardBodyColumnElement = document.createElement('div');
+        cardBodyColumnElement.classList.add('col-md-8');
+        // Declare and initialize a variable to store and create a Bootstrap card title HTML element.
+        let cardTitleElement = document.createElement('h5');
+        cardTitleElement.classList.add('card-title');
+        // Add the TMDB Title to the card title HTML element.
+        if (titleSearchTypeField.value === 'movie') {
+            cardTitleElement.textContent = result.title;
+        } else if (titleSearchTypeField.value === 'tv') {
+            cardTitleElement.textContent = result.name;
+        }
+        // Declare and initialize a variable to store and create a Bootstrap card subtitle HTML element.
+        let cardSubtitleElement = document.createElement('h6');
+        cardSubtitleElement.classList.add('card-subtitle');
+        // Add the TMDB Vote Average and Vote Count to the card subtitle HTML element.
+        cardSubtitleElement.textContent = `${result.vote_average} out of ${result.vote_count} votes`;
+        // Declare and initialize a variable to store and create a Bootstrap card text HTML element.
+        let cardTextElement = document.createElement('p');
+        cardTextElement.classList.add('card-text');
+        // Add the TMDB Overview to the card text HTML element.
+        // TODO: Limit the amount of displayed text so as to not expand the card.
+        cardTextElement.textContent = result.overview;
+        // TODO Declare and initialize a variable to store and create a Bootstrap card button HTML element to allow the user to add the movie/show to their list of movies/shows to watch.
+        let cardWatchButtonElement = document.createElement('button');
+        cardWatchButtonElement.classList.add('btn');
+        cardWatchButtonElement.classList.add('btn-warning');
+        cardWatchButtonElement.setAttribute('type', 'button');
+        cardWatchButtonElement.textContent = 'Add to Watch List';
+        // Declare and initialize a variable to store and create a Bootstrap card button HTML element to allow the user to add the movie/show to their list of watched movies/shows.
+        let cardWatchedButtonElement = document.createElement('button');
+        cardWatchedButtonElement.classList.add('btn');
+        cardWatchedButtonElement.classList.add('btn-warning');
+        cardWatchedButtonElement.setAttribute('type', 'button');
+        cardWatchedButtonElement.textContent = 'Mark as Watched';
+        // Append the individual card elements to the search results container.
+        searchResultContainer.appendChild(cardElement);
+        cardElement.appendChild(cardRowElement);
+        cardRowElement.appendChild(cardImageColumnElement);
+        cardImageColumnElement.appendChild(cardImageElement);
+        cardRowElement.appendChild(cardBodyColumnElement);
+        cardBodyColumnElement.appendChild(cardTitleElement);
+        cardBodyColumnElement.appendChild(cardSubtitleElement);
+        cardBodyColumnElement.appendChild(cardTextElement);
+        cardBodyColumnElement.appendChild(cardWatchButtonElement);
+        cardBodyColumnElement.appendChild(cardWatchedButtonElement);
     }
 }
 
